44ef44c2c011db69ae3d3af9572d9a7866a6f670
sovadim <vadik.sofin@yandex.ru>
2022-06-04 16:02:58 +0300

    Add proj* to formatting paths

--- apply_format.sh
+++ apply_format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # C++
-find lec* hw* -iname "*.h" -o -iname "*.c" | xargs clang-format -i
+find lec* hw* proj* -iname "*.h" -o -iname "*.c" | xargs clang-format -i
 
 # Python
 python3 -m black .
